173f84baab77a09183fd2ca64723e23f5a0a330d
Divyajyoti Patra <djpatra07@gmail.com>
2024-02-17 20:38:38 +0530

    Create Generate_Txt_File.py

---
+++ Testing Py files/Generate_Txt_File.py
@@ -0,0 +1,6 @@
+# Create a text file and store data
+with open('data.txt', 'w') as file:
+    file.write('Temperature: 25\n')
+    file.write('Humidity: 50\n')
+    file.write('Pressure: 1013\n')
+    file.write('Location: Home\n')
